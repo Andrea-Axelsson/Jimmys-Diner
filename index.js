919a47d602795f4df95248a921f7f2ce3e9a3aa7
Andrea Axelsson <120781985+Andrea-Axelsson@users.noreply.github.com>
2023-10-12 13:10:37 +0200

    Updated code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {menuArray} from './data.js'
+import { menuArray } from './data.js'
 const completeOrderBtn = document.getElementById('complete-order-btn')
 const payBtn = document.getElementById('pay-btn')
 const exitArrow = document.getElementById('exit-arrow')
@@ -12,88 +12,88 @@ render()
 completeOrderBtn.addEventListener('click', openModal)
 exitArrow.addEventListener('click', closeModal)
 
-function openModal(){
+function openModal() {
     modalBody.style.display = 'flex'
 }
 
-function closeModal(){
+function closeModal() {
     modalBody.style.display = 'none'
 }
 
 document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  document.getElementById('order-container').style.display = 'none'
-  thankYouModal.style.display = 'flex'
-   modalBody.style.display = 'none'
-   
-   setTimeout(function() {
-  thankYouModal.style.display = 'none';
-  location.reload(); 
-}, 3000);
+    e.preventDefault();
 
-   
+    document.getElementById('order-container').style.display = 'none'
+    thankYouModal.style.display = 'flex'
+    modalBody.style.display = 'none'
+
+    setTimeout(function () {
+        thankYouModal.style.display = 'none';
+        location.reload();
+    }, 3000);
+
+
 });
 
 
-document.addEventListener('click', function(e){
-   if (e.target.dataset.addItem){
-       handleAddItemClick(e.target.dataset.addItem)
-   } else if (e.target.dataset.removeItem){
-       handleRemoveItemClick(e.target.dataset.removeItem)
-   } else if (e.target.dataset.minusItem){
-       handleRemoveItemClick(e.target.dataset.minusItem)
-   }
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.addItem) {
+        handleAddItemClick(e.target.dataset.addItem)
+    } else if (e.target.dataset.removeItem) {
+        handleRemoveItemClick(e.target.dataset.removeItem)
+    } else if (e.target.dataset.minusItem) {
+        handleRemoveItemClick(e.target.dataset.minusItem)
+    }
 })
 
-function handleAddItemClick(addItemId){
-   const targetMenuObj = menuArray.filter(function(dish){
-       return dish.id === addItemId;
-   })[0]
-       targetMenuObj.isOrdered = true
-       targetMenuObj.nrOfTimesOrdred ++
-       document.getElementById('order-container').style.display =  'flex'
-       window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
-       
-      
-       render()
+function handleAddItemClick(addItemId) {
+    const targetMenuObj = menuArray.filter(function (dish) {
+        return dish.id === addItemId;
+    })[0]
+    targetMenuObj.isOrdered = true
+    targetMenuObj.nrOfTimesOrdred++
+    document.getElementById('order-container').style.display = 'flex'
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
+
+
+    render()
 }
 
-function handleRemoveItemClick(removeItemId){
-    const targetOrderedDishObj = menuArray.filter(function(dish){
+function handleRemoveItemClick(removeItemId) {
+    const targetOrderedDishObj = menuArray.filter(function (dish) {
         return dish.id === removeItemId
     })[0]
-    if (targetOrderedDishObj.nrOfTimesOrdred <= 1){
+    if (targetOrderedDishObj.nrOfTimesOrdred <= 1) {
         targetOrderedDishObj.isOrdered = false
         targetOrderedDishObj.nrOfTimesOrdred = 0
         console.log(targetOrderedDishObj.nrOfTimesOrdred)
-        
-    }else {
-        targetOrderedDishObj.nrOfTimesOrdred --
+
+    } else {
+        targetOrderedDishObj.nrOfTimesOrdred--
         console.log(targetOrderedDishObj.nrOfTimesOrdred)
     }
-    
-    
+
+
     window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
     render()
 }
 
 
 
-function getFeedHtml(){
- let feedHtml = ``;
- let dishOrderArray = []
- const minusBtn = document.getElementById('minus-btn')
+function getFeedHtml() {
+    let feedHtml = ``;
+    let dishOrderArray = []
+    const minusBtn = document.getElementById('minus-btn')
 
- // Loopa genom varje maträtt
- menuArray.forEach(function(dish) {
-     
-      if (dish.isOrdered){
-        dishOrderArray.push(dish)   
-    }
+    // Loopa genom varje maträtt
+    menuArray.forEach(function (dish) {
+
+        if (dish.isOrdered) {
+            dishOrderArray.push(dish)
+        }
 
 
-   feedHtml += `<section>
+        feedHtml += `<section>
      <div class="food-container">
        <p class="food-emoji">${dish.emoji}</p>
        <div class="item-info">
@@ -110,31 +110,29 @@ function getFeedHtml(){
        </div>
      </div>
    </section>`;
- });
+    });
 
 
-if(dishOrderArray.length > 0)
-{
-    document.getElementById('order').innerHTML = getOrderHtml(dishOrderArray)
-} else if (dishOrderArray.length < 1){
-    document.getElementById('order-container').style.display =  'none'
-    
-}
+    if (dishOrderArray.length > 0) {
+        document.getElementById('order').innerHTML = getOrderHtml(dishOrderArray)
+    } else if (dishOrderArray.length < 1) {
+        document.getElementById('order-container').style.display = 'none'
+
+    }
 
     return feedHtml;
 }
 
 
-function getOrderHtml(dishOrderArray){
+function getOrderHtml(dishOrderArray) {
     let orderHtml = ``
     let totalCost = 0
     let totalDishPrice = 0
     let totalItemOrder = 0
 
-    
-    dishOrderArray.forEach(function(dish)
-    {
-        
+
+    dishOrderArray.forEach(function (dish) {
+
         totalDishPrice = dish.price * dish.nrOfTimesOrdred
         totalCost += totalDishPrice
         orderHtml += `
@@ -143,16 +141,16 @@ function getOrderHtml(dishOrderArray){
             <button class="remove-btn" data-remove-item="${dish.id}">remove</button>
             <p><strong>$${totalDishPrice}</strong></p>
             </div>
-         `      
+         `
     })
-    
+
     document.getElementById('total-cost').innerHTML = '$' + totalCost
 
     return orderHtml
-}   
+}
 
 
-function render(){
-   document.getElementById('food-container').innerHTML =  getFeedHtml()
-    
+function render() {
+    document.getElementById('food-container').innerHTML = getFeedHtml()
+
 }                 
